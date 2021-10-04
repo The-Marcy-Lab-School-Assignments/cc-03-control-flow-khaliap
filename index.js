9b3ee564d781defdeec0e93230dfc2d36c063f60
khalia <khaliaparris01@gmail.com>
2021-10-04 14:22:32 +0000

    answered questions 1-5 for code challenge

--- index.js
+++ index.js
@@ -1,25 +1,41 @@
 //1
-function countToTen(){
-  
+function countFromOne(num){
+  for (let i =1; i <= num; i ++)
+  console.log(i)
 }
-
+//countFromOne(5)
 //2
-function countFromOne(){
-  
+function countEveryOdd(num1){
+    for(let i = 1; i <num1; i++){
+      if(i%2 !== 0){
+       console.log(i)   
+      }
 }
-
+}
+//countEveryOdd(10)
 //3
-function countEveryEven(){
-  
+function isNegative(number){
+  if(number<0){
+      return true 
+  }else {
+}return false
 }
-
+//console.log(isNegative(-5))
 //4
-function countEveryOdd(){
-  
+function betweenFiveAndTwenty(num){
+    if(num>=5 && num<=20){
+  return true
+}else{
+    return false
 }
-
+}
+//console.log(betweenFiveAndTwenty(10))
 //5 
-function countEvens(){
-  
+function isAllLowerCase(string){
+  if(string === string.toLowerCase()){
+      return true
+  }else {
+      return false
+  }
 }
-
+console.log(isAllLowerCase())
